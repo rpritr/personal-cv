8bc49aa2bf7fbe33f4d8f505469303fc4db7e707
rpritr <robi@foreach-labs.si>
2024-04-14 09:41:42 +0200

    Update Webpage to use SCSS

--- src/js/index.js
+++ src/js/index.js
@@ -4,6 +4,8 @@ import { createRoot } from "react-dom/client";
 const container = document.getElementById("app");
 const root = createRoot(container);
 
+import '../scss/_variables.scss';
+import '../scss/main.scss';
 const App = () => {
     return (
        <h1>Hello!</h1>
