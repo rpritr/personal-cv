36c7230cdb2776ce800d13ba8a3daa9e30af9bf5
rpritr <robi@foreach-labs.si>
2024-04-14 10:08:12 +0200

    Add Components structure

---
+++ src/js/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "./Components/Header";
+import '../scss/_variables.scss';
+import '../scss/main.scss';
+
+const App = () => {
+    return (
+        <>
+            <Header/>
+        </>
+        )
+}
+
+export default App;
--- src/js/index.js
+++ src/js/index.js
@@ -1,14 +1,7 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-
 const container = document.getElementById("app");
 const root = createRoot(container);
+import App from "./App";
 
-import '../scss/_variables.scss';
-import '../scss/main.scss';
-const App = () => {
-    return (
-       <h1>Hello!</h1>
-    )
-}
 root.render(<App/>);
